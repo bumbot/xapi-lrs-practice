ce990b5a1adcc4cf893d0b513433445ab54bd231
bumbot <pkim1295@gmail.com>
2020-12-08 14:21:11 -0500

    skateboard

--- README.md
+++ README.md
@@ -4,22 +4,23 @@ A basic react app to test out writing xAPI statements and LRS functionality. Use
 
 **Validated** statements can be viewed at http://adlnet.github.io/xapi-statement-viewer/
 
+Pulls cryptojs_v3.1.2.js & xapiwrapper.js from the xAPIWrapper library
+
 
 ***
 
 
-## Getting Started with Create React App
+## Installation
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-## Available Scripts
-
-In the project directory, you can run:
-
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+1. Clone this repo
+```
+    git clone https://github.com/bumbot/xapi-lrs-practice
+```
+2. Install the modules to create the package-lock file
+```
+    npm install
+```
+3. Start the server
+```
+    npm start
+```
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,33 +1,87 @@
 import React, {Component} from 'react';
+// import API from './cryptojs_v3.1.2'
+// import xAPIWrapper from './xapiwrapper'
 
-const LRS = 'https://lrs.adlnet.gov/xapi'
+/*
+    because we added cryptojs and xapiwrapper files as script tags in index.html
+    we can now call on functions inside of the respective files by prepending
+    each function with 'window.functionName'
+*/
 
 class Main extends Component {
     
     //generates and sends xAPI statement
-    sendStatement(event) {
+    sendAPIStatement(event) {
         let actor = this.props.userLogin,
-            verb = this.props.userAction,
-            activity = this.props.userActivity
+            verb = event.target.value,
+            activity = event.target.dataset.object,
+            description = event.target.dataset.description
 
-        let action = event.target;
-        debugger
-        //depending on action,  populate statement
+        let statement;
 
-                /* generic button pressed, action turns to clicked*/
-        if (action = "button") {
+        //configure endpoints
+        var config = {
+            "endpoint" : "https://lrs.adlnet.gov/xapi/",
+            "auth" : "Basic " + window.toBase64("xapi-tools:xapi-tools")
+            };
+ 
+        window.ADL.XAPIWrapper.changeConfig(config);
 
-                /* button under video pressed, action turns to watched */
-        } else if (action = "") {
+        //depending on action, populate statement
+        
+        /* generic button pressed, action turns to clicked*/
+        if (verb === "clicked") {
+            statement = this.populateStatement(actor, verb, activity);
+
+            // this.setState({
+            //     userAction: verb,
+            //     userActivity: activity
+            // })
+
+        /* button under video pressed, action turns to watched */
+        } else if (verb === "watched") {
+            statement = this.populateStatement(actor, verb, activity)
+
+            // this.setState({
+            //     userAction:verb,
+            //     userActivity: activity
+            // })
 
         } else {
             alert("Unidentified Action");
+            statement = this.populateStatement(actor, "n/a", activity)
         }
-
+        
+        //send info to lrs
+        var result = window.ADL.XAPIWrapper.sendStatement(statement);
+        
+        //outputs alert - if error occured, display error; else display success
+        result.xhr.status === 400 ? alert(result.xhr.responseText) : alert("xAPI statment successfully sent to LRS!")
+        
 
     }
 
-    populateStatement
+    populateStatement(actor, verb, activity) {
+        return {
+            "actor": {
+                "mbox": `mailto:${actor}@example.com`,
+                "name": `${actor}`,
+                "objectType": "Agent"
+            },  
+            "verb": {
+                "id": "http://example.com/xapi/interacted",
+                "display": {"en-US": `${verb}`}
+            },
+            "object": {
+                "id": "http://example.com/button_example",
+                "definition": {
+                    "name": {"en-US": `${activity}`},
+                    "description": {"en-US": `${activity}`}
+                },
+                "objectType": "Activity"
+            }
+        }
+    }
 
     //what gets rendered once user gets past login page
     render(){
@@ -35,20 +89,27 @@ class Main extends Component {
             <div>
                 {/* generic button: action = "clicked" */}
                 <div>
-                    <button onClick={(event)=> this.sendStatement(event)}>
+                    <button value="clicked"
+                            data-object="button"
+                            data-description="Pressed submit button"
+                            onClick={(event)=> this.sendAPIStatement(event)}>
                         Click here to send a sample xAPI statement
                     </button>
                 </div>
                 {/* video button: action = "watched" */}
                 <div>
-                    <iframe width="560"
+                    <iframe title="Experience API"
+                            width="560"
                             height="315"
                             src="https://www.youtube.com/embed/y42MSS1DJqc"
                             frameBorder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                             allowFullScreen
                     />
-                    <button onClick={(event) => this.sendStatement(event)}>
+                    <button value="watched"
+                            data-object="xapi video"
+                            data-description="Watched the Experience API video"
+                            onClick={(event) => this.sendAPIStatement(event)}>
                         Click here once finished watching
                     </button>
                 </div>
@@ -67,7 +128,8 @@ export default Main;
 
 /*
     xAPI Statement definition:
-
+    
+    {
     "actor": {  
         "mbox": "mailto:Tester@example.com",  
         "name": "tester12",  
